c3d892b66d226663bc0aa1e1e4488eddab7fcc0b
jhonathan25111998-cyber <jhonathan25111998@gmail.com>
2026-05-30 12:55:59 -0300

    Add files via upload

---
+++ sj.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'fluxo-v1';
+const urlsToCache = ['/', '/index.html', '/logo.png', '/manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/logo.png',
+      badge: '/icon-72.png'
+    })
+  );
+});
